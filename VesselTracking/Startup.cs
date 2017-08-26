70a0fb2f386ef3b4cf37e95c118a1cca1115de05
Justyna <Justyna@JUSTYS>
2017-08-26 16:05:19 +0100

    Add project files.

---
+++ VesselTracking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VesselTracking.Startup))]
+namespace VesselTracking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
